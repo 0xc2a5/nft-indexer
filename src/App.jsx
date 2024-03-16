c1212282e83cb6bc7ea7f73bf10d784a3c3a5b73
0xc2a5 <155046752+0xc2a5@users.noreply.github.com>
2024-03-16 15:36:02 +0900

    Complete task

--- src/App.jsx
+++ src/App.jsx
@@ -8,46 +8,104 @@ import {
   Input,
   SimpleGrid,
   Text,
-} from '@chakra-ui/react';
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useState } from 'react';
+} from "@chakra-ui/react";
+import { Alchemy, Network } from "alchemy-sdk";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import Fallback from "./Fallback";
+import classes from "./App.module.css";
 
 function App() {
-  const [userAddress, setUserAddress] = useState('');
+  const [userAddress, setUserAddress] = useState("");
   const [results, setResults] = useState([]);
   const [hasQueried, setHasQueried] = useState(false);
+  const [queries, setQueries] = useState({});
   const [tokenDataObjects, setTokenDataObjects] = useState([]);
+  const [alchemy, setAlchemy] = useState();
+  const [loading, setLoading] = useState(false);
 
-  async function getNFTsForOwner() {
-    const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
-      network: Network.ETH_MAINNET,
-    };
+  useEffect(() => {
+    setAlchemy(new Alchemy({
+      apiKey: import.meta.env.VITE_ALCHEMY_API_KEY_SEPOLIA,
+      network: Network.ETH_SEPOLIA
+    }));
+  }, []);
 
-    const alchemy = new Alchemy(config);
-    const data = await alchemy.nft.getNftsForOwner(userAddress);
-    setResults(data);
+  useEffect(() => {
+    async function connectWallet() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
 
-    const tokenDataPromises = [];
+      if (address && alchemy) {
+        setUserAddress(address);
+        await getNFTsForOwner(address);
+      }
+    }
+    connectWallet();
+  }, [alchemy]);
 
-    for (let i = 0; i < data.ownedNfts.length; i++) {
-      const tokenData = alchemy.nft.getNftMetadata(
-        data.ownedNfts[i].contract.address,
-        data.ownedNfts[i].tokenId
-      );
-      tokenDataPromises.push(tokenData);
+  async function handleCachedQuery(address) {
+    setUserAddress(address);
+    await getNFTsForOwner(address);
+  }
+
+  async function getNFTsForOwner(address) {
+    const _address = address || userAddress;
+    const cachedQuery = queries[_address];
+
+    if (!_address) {
+      setHasQueried(false);
+      return;
+    }
+    else if (cachedQuery) {
+      setResults(cachedQuery.data);
+      setTokenDataObjects(cachedQuery.tokenDataObjects);
+      setHasQueried(true);
+      return;
     }
 
-    setTokenDataObjects(await Promise.all(tokenDataPromises));
-    setHasQueried(true);
+    setLoading(true);
+
+    try {
+      const data = await alchemy.nft.getNftsForOwner(_address);
+      setResults(data);
+
+      const tokenDataPromises = [];
+      for (let i = 0; i < data.ownedNfts.length; i++) {
+        const tokenData = alchemy.nft.getNftMetadata(
+          data.ownedNfts[i].contract.address,
+          data.ownedNfts[i].tokenId
+        );
+        tokenDataPromises.push(tokenData);
+      }
+
+      const tokenDataObjects = await Promise.all(tokenDataPromises);
+
+      setQueries(queries => ({
+        [_address]: {
+          data,
+          tokenDataObjects,
+        },
+        ...queries
+      }));
+      setTokenDataObjects(tokenDataObjects);
+      setHasQueried(true);
+    }
+    catch (error) {
+      console.log(error);
+      alert("Failed to check ERC-721 tokens.");
+    }
+    setLoading(false);
   }
   return (
     <Box w="100vw">
       <Center>
         <Flex
-          alignItems={'center'}
+          alignItems={"center"}
           justifyContent="center"
-          flexDirection={'column'}
+          flexDirection={"column"}
         >
           <Heading mb={0} fontSize={36}>
             NFT Indexer 🖼
@@ -61,11 +119,15 @@ function App() {
         w="100%"
         flexDirection="column"
         alignItems="center"
-        justifyContent={'center'}
+        justifyContent={"center"}
       >
         <Heading mt={42}>Get all the ERC-721 tokens of this address:</Heading>
         <Input
+          className={classes.input}
           onChange={(e) => setUserAddress(e.target.value)}
+          value={userAddress}
+          maxLength={42}
+          autoFocus
           color="black"
           w="600px"
           textAlign="center"
@@ -73,43 +135,56 @@ function App() {
           bgColor="white"
           fontSize={24}
         />
-        <Button fontSize={20} onClick={getNFTsForOwner} mt={36} bgColor="blue">
-          Fetch NFTs
+        <Button fontSize={20} onClick={() => getNFTsForOwner()} mt={36} bgColor="blue" isDisabled={loading}>
+          {loading
+            ? "Loading..."
+            : "Fetch NFTs"
+          }
         </Button>
 
         <Heading my={36}>Here are your NFTs:</Heading>
 
         {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
+          <SimpleGrid w={"90vw"} columns={4} spacing={24}>
             {results.ownedNfts.map((e, i) => {
+              console.log("image", tokenDataObjects[i]?.rawMetadata?.image);
               return (
                 <Flex
-                  flexDir={'column'}
+                  className={classes.gridItem}
+                  flexDir={"column"}
                   color="white"
                   bg="blue"
-                  w={'20vw'}
-                  key={e.id}
+                  w={"20vw"}
+                  key={`${e.id}-${i}`}
                 >
-                  <Box>
-                    <b>Name:</b>{' '}
-                    {tokenDataObjects[i].title?.length === 0
-                      ? 'No Name'
-                      : tokenDataObjects[i].title}
+                  <Box className={classes.name}>
+                    <b>Name:</b>{" "}
+                    {tokenDataObjects[i]?.title?.length === 0
+                      ? "No Name"
+                      : tokenDataObjects[i]?.title}
                   </Box>
                   <Image
-                    src={
-                      tokenDataObjects[i]?.rawMetadata?.image ??
-                      'https://via.placeholder.com/200'
-                    }
-                    alt={'Image'}
+                    className={classes.image}
+                    fallback={<Fallback />}
+                    src={tokenDataObjects[i]?.rawMetadata?.image || ""}
+                    onLoad={() => console.log("load")}
+                    onError={() => console.log("error")}
+                    alt={"Image"}
                   />
                 </Flex>
               );
             })}
           </SimpleGrid>
         ) : (
-          'Please make a query! The query may take a few seconds...'
+          "Please make a query! The query may take a few seconds..."
         )}
+
+        <Heading my={36}>Recent queries:</Heading>
+        <Flex className={classes.history} direction={"column"}>
+          {Object.keys(queries).map(address => (
+            <a key={address} onClick={() => handleCachedQuery(address)}>{address}</a>
+          ))}
+        </Flex>
       </Flex>
     </Box>
   );
---
+++ src/Fallback.jsx
@@ -0,0 +1,5 @@
+import classes from "./Fallback.module.css";
+
+export default function Fallback() {
+    return <div className={classes.fallback}>Loading...</div>;
+}
